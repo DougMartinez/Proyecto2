de211d6ffc177276827eafee617509e261577e59
DougMartinez <douglas.lxv@gmail.com>
2017-11-10 19:04:04 -0600

    ejemplo

--- src/proyecto2/PackProcesor.java
+++ src/proyecto2/PackProcesor.java
@@ -28,7 +28,7 @@ public class PackProcesor {
                 try{
                     listaprod.getValor(id - 1).setCantidad( listaprod.getValor(id - 1).getCantidad() + cantidad);
                 } catch(Exception e){
-                    
+//                    Esto no estaba antes
                 }
             }
         };
